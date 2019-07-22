ff86131db70bbf15dca434a28911f714d54f290f
zhangziqi1988 <zhangziqi1988@126.com>
2019-07-22 21:31:06 +0800

    添加买家模块

---
+++ src/main/java/com/imooc/sell/VO/ResultVO.java
@@ -0,0 +1,17 @@
+package com.imooc.sell.VO;
+
+import lombok.Data;
+
+@Data
+public class ResultVO<T> {
+
+    //错误码
+    private Integer code;
+
+    //提示信息
+    private String msg;
+
+    //返回的具体内容
+    private T data;
+
+}
